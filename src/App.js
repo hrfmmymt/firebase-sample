f32cb30e223782d6dc5e9fb287d53c8f1944225b
hrfmmymt <hrfmmymt@gmail.com>
2017-07-08 12:39:28 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ class App extends Component {
   }
 
   login() {
-    auth.signInWithPopup(provider)
+    auth.signInWithPopup(provider) 
       .then((result) => {
-        const user = result.user
+        const user = result.user;
         this.setState({
           user
         })
@@ -47,7 +47,7 @@ class App extends Component {
     const itemsRef = firebase.database().ref('items')
     const item = {
       title: this.state.currentItem,
-      user: this.state.username
+      user: this.state.user.displayName || this.state.user.email
     }
     itemsRef.push(item)
     this.setState({
@@ -58,7 +58,9 @@ class App extends Component {
 
   componentDidMount() {
     auth.onAuthStateChanged((user) => {
-      if (user) this.setState({user})
+      if (user) {
+        this.setState({ user })
+      } 
     })
 
     const itemsRef = firebase.database().ref('items')
@@ -88,46 +90,49 @@ class App extends Component {
     return (
       <div className='app'>
         <header>
-            <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
-              {this.state.user ?
-                <button onClick={this.logout}>Log Out</button>
-                :
-                <button onClick={this.login}>Log In</button>
-              }
-            </div>
+          <div className='wrapper'>
+            <h1>Fun Food Friends</h1>
+            {this.state.user ?
+              <button onClick={this.logout}>Logout</button>                
+            :
+              <button onClick={this.login}>Log In</button>              
+            }
+          </div>
         </header>
         {this.state.user ?
-          <div>
-            <div className='user-profile'>
-              <img src={this.state.user.photoURL} />
-            </div>
+        <div>
+          <div className='user-profile'>
+            <img src={this.state.user.photoURL} />
           </div>
-          :
-          <div className='wrapper'>
-            <p>You must be logged in to see the potluck list and submit to it.</p>
-          </div>
+        </div>
+        :
+        <div className='wrapper'>
+          <p>You must be logged in to see the potluck list and submit to it.</p>
+        </div>
         }
         <div className='container'>
           <section className='add-item'>
-              <form onSubmit={this.handleSubmit}>
-                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                <button>Add Item</button>
-              </form>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" name="username" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} />
+              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+              <button>Add Item</button>
+            </form>
           </section>
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
-                {this.state.items.map((item) => {
-                  return(
-                    <li key={item.id}>
-                      <h3>{item.title}</h3>
-                      <p>brought by: {item.user}</p>
-                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                    </li>
-                  )
-                })}
+              {this.state.items.map((item) => {
+                return(
+                  <li key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>brought by: {item.user}
+                      {item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
+                    </p>
+                    <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                  </li>
+                )
+              })}
               </ul>
             </div>
           </section>
